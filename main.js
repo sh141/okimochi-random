3954e4d1919f41cf3ec89fca750087fd89329ae5
松坂淳生 <a.matsuzaka@opt.ne.jp>
2019-06-10 18:12:56 +0900

    リフレッシュ機能に自動リフレッシュを追加

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const content = document.getElementById("content"); //root content
 
 class OkimochiCharacter {
-    constructor(character){
+    constructor(character) {
         this.character = character;
         this.node = document.createElement("div");
         this.node.textContent = character;
@@ -13,22 +13,23 @@ class OkimochiCharacter {
 }
 
 class OkimochiContainer {
-    constructor(...args){
+    constructor(...args) {
         this.node = document.createElement("div");
         content.appendChild(this.node);
 
-        for(let i = 0; i < args.length; i++){
+        for (let i = 0; i < args.length; i++) {
             this.node.appendChild(args[i].node);
         }
     }
 
-    choiceStyle(){
-        const styleList = ["row","row-reverse","column","column-reverse"];
+    choiceStyle() {
+        const styleList = ["row", "row-reverse", "column", "column-reverse"];
         const className = "okimochi " + styleList[Math.floor(Math.random() * styleList.length)];
         this.node.className = className;
         const rand = Math.random() * 3;
-        if(rand < 1) this.node.style.alignItems = "center";
-        else if(rand < 2) this.node.style.alignItems = "flex-end";
+        if (rand < 1) this.node.style.alignItems = "center";
+        else if (rand < 2) this.node.style.alignItems = "flex-end";
+
     }
 }
 
@@ -36,8 +37,9 @@ const O = new OkimochiCharacter("お")
 const KI = new OkimochiCharacter("き")
 const MO = new OkimochiCharacter("も")
 const CHI = new OkimochiCharacter("ち")
-const allOKIMOCHI = new OkimochiContainer(O,KI,MO,CHI)
+const allOKIMOCHI = new OkimochiContainer(O, KI, MO, CHI)
 allOKIMOCHI.choiceStyle();
 
+const okimochiRefresh = () => setInterval(allOKIMOCHI.choiceStyle.bind(allOKIMOCHI), 5000)
 //"お"をクリックでリフレッシュ
-O.node.addEventListener("click",allOKIMOCHI.choiceStyle.bind(allOKIMOCHI),false);
+O.node.addEventListener("click", okimochiRefresh, false);
